4e75e0290705e5180e9f64892c0b4f49d8e1866a
javierpentola <javiergraciafn@gmail.com>
2024-06-09 10:24:48 +0200

    Mensaje descriptivo de tu commit

--- pages/login/check_login_status.php
+++ pages/login/check_login_status.php
@@ -4,6 +4,6 @@ session_start();
 if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] === true) {
     echo "Estás logeado.";
 } else {
-    echo "No estás logeado.";
+    echo "No estás logeado";
 }
 ?>
--- pages/login/checklogin.php
+++ pages/login/checklogin.php
@@ -6,7 +6,7 @@ $session_duration = 3600;
 
 // Verificar si la sesión está configurada y si ha expirado
 if (isset($_SESSION['loggedin_time']) && (time() - $_SESSION['loggedin_time'] > $session_duration)) {
-    echo "Sesión expirada. Destruyendo sesión.";
+    // Si la sesión ha expirado, destruirla
     session_unset();
     session_destroy();
     header('Location: /College%20campus/pages/login/index.php');
@@ -14,11 +14,9 @@ if (isset($_SESSION['loggedin_time']) && (time() - $_SESSION['loggedin_time'] > 
 } else {
     // Si la sesión no ha expirado, actualizar el tiempo de sesión
     if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] === true) {
-        echo "Sesión activa. Actualizando tiempo de sesión.";
         $_SESSION['loggedin_time'] = time();
     } else {
         // Redirigir al login si no está logeado
-        echo "No estás logeado. Redirigiendo al login.";
         header('Location: /College%20campus/pages/login/index.php');
         exit();
     }
